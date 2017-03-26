891c7b2a1d55fad75ea34a369a2f5acad5a0ffb0
murad_khateeb <muradkhateeb788@gmail.com>
2017-03-26 21:35:02 +0500

    I have added the source code

---
+++ src/database.java
@@ -0,0 +1,9 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class database {
+	public ArrayList<ArrayList> database = new ArrayList<>();
+	public void checking(){
+		
+	}
+}
---
+++ src/keyInNode.java
@@ -0,0 +1,16 @@
+
+public class keyInNode {
+	private int index;
+	public leafNode lNode;
+	public keyInNode(leafNode lNode,int index){
+		this.index=index;
+		this.lNode=lNode;
+	}
+	public int getIndex() {
+		return index;
+	}
+	public void setIndex(int index) {
+		this.index = index;
+	}
+
+}
