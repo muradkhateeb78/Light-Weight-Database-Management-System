2eefe4bee7a1c4b29c6f2315879831b4e4ae6f36
shahrukhmk <shahrukhkhoso@outlook.com>
2017-03-27 21:45:28 +0500

    Lite Parser implemeneted for simple queries for our database. BTree.java receives all parsed data; where we will be checking whether the attributes and tables in query exist. After checking, we run the query.

---
+++ Parser/DeleteQueryTokens.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class DeleteQueryTokens {
+
+	private String relation;
+	private ArrayList<Qualifiers> qualifierList;
+	private ArrayList<String> andOr;
+	
+	public DeleteQueryTokens(){
+		relation = null;
+		qualifierList = new ArrayList<>();
+		andOr = new ArrayList<>();
+	}
+
+	public String getRelation() {
+		return relation;
+	}
+
+	public void setRelation(String relation) {
+		this.relation = relation;
+	}
+
+	public ArrayList<Qualifiers> getQualifierList() {
+		return qualifierList;
+	}
+
+	public void setQualifierList(ArrayList<Qualifiers> qualifierList) {
+		this.qualifierList = qualifierList;
+	}
+
+	public ArrayList<String> getAndOr() {
+		return andOr;
+	}
+
+	public void setAndOr(ArrayList<String> andOr) {
+		this.andOr = andOr;
+	}
+}
