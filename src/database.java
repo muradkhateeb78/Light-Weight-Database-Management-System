a5f6d0db2366f83ff791f77ccee8e8c8d6d8989c
murad_khateeb <muradkhateeb788@gmail.com>
2017-05-02 00:12:22 +0500

    I have deleted all the files and i will be uploading the updated version of the code sooner

--- src/database.java
+++
@@ -1,9 +0,0 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-
-public class database {
-	public ArrayList<ArrayList> database = new ArrayList<>();
-	public void checking(){
-		
-	}
-}
